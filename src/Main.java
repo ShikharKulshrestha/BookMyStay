1531a76f3e6debc6367008a7608ce621d8aa2418
ShikharKulshrestha <sk7884@srmist.edu.in>
2026-03-10 08:41:12 +0530

    UC1: Add welcome message module

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System Initialized Successfully");
+
+    }
+}
